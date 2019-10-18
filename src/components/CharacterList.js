66deb5541a1229e850cff8224b48ee97bc4ec302
Ale <alearanibar@gmail.com>
2019-10-18 08:33:46 -0700

    have state

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [character, setCharacter] = useState()
+  let url1 = `https://rickandmortyapi.com/api/character/`
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get(url1)
+      .then(response => {
+        let newData = response.data.results
+        console.log(newData)
+      })
   }, []);
 
   return (
